2ceb424df83fefe720240b8738700390c06e6a3c
Soham Biswas <sohambis.in@gmail.com>
2026-01-27 00:41:38 +0100

    9.palindromeNumber.java

--- 9.palindromeNumber.java
+++ 9.palindromeNumber.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public boolean isPalindrome(int x) {
+        int n,temp=0,d;
+        n=x;
+        if (x < 0 || (x % 10 == 0 && x != 0)) 
+            return false;
+        while(n!=0)
+        {
+            d=n%10;
+            temp=temp*10+d;
+            n=n/10;
+        }
+        if(temp==x)
+            return true;
+        else
+            return false;
+    }
+}
